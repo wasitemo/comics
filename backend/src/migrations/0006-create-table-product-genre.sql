85aa44a5c33b3ad2bf667a7af1d0eff6b8d98e6d
Navyrine <euphorigenic990@gmail.com>
2026-03-06 10:44:27 +0800

    memperbaiki error syntax pada pembuatan table product genre

--- backend/src/migrations/0006-create-table-product-genre.sql
+++ backend/src/migrations/0006-create-table-product-genre.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS product_genre (
     product_id INT REFERENCES product (product_id) ON DELETE CASCADE,
-    genre_id INT REFERENCES genre (genre_id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genre (genre_id) ON DELETE CASCADE
 
 )

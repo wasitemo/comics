4acfa472c41ee2d3a58719a4464ef7366a03abe1
Navyrine <euphorigenic990@gmail.com>
2026-03-06 10:38:49 +0800

    menghapus primary key pada table product genre

--- backend/src/migrations/0006-create-table-product-genre.sql
+++ backend/src/migrations/0006-create-table-product-genre.sql
@@ -2,5 +2,4 @@ CREATE TABLE IF NOT EXISTS product_genre (
     product_id INT REFERENCES product (product_id) ON DELETE CASCADE,
     genre_id INT REFERENCES genre (genre_id) ON DELETE CASCADE,
 
-    PRIMARY KEY (product_id, genre_id)
 )
--- backend/src/migrations/0007-delete-constraint-table-product-genre.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE product_genre
-DROP CONSTRAINT product_id
-DROP CONSTRAINT genre_id
